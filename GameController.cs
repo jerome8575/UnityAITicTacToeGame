862e52a6727f9aa35df3afc9b7c6493d1022e543
Jerome Emery <58118521+jerome8575@users.noreply.github.com>
2020-06-20 14:56:43 -0700

    Two scripts controlling the game

---
+++ GameController.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameController : MonoBehaviour
+{
+    public Text[] buttonList;
+    public GameObject gameOverPanel;
+    public Text gameOverText;
+
+    private string playerSide;
+    private int moveCount;
+    private string[,] board = new string[3, 3];
+    private bool isTie;
+
+    struct Move
+    {
+        public int row;
+        public int col;
+    }
+
+    public void setMyArray()
+    {
+        // check which button was pressed and make that play on our array board
+        int index = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                board[i, j] = buttonList[index].text;
+                index++;
+            }
+        }
+    }
+    public void makeAIPlay()
+    {
+        Move Aimove = findBestMove();
+        int buttonlistindex = Aimove.col + Aimove.row * 3;
+        buttonList[buttonlistindex].GetComponentInParent<Button>().interactable = false;
+        buttonList[buttonlistindex].text = "O";
+        EndTurn();
+    }
+    public string getPlayerSide()
+    {
+        return playerSide;
+    }
+    public void EndTurn()
+    {
+        // checkWin
+        moveCount++;
+        if (buttonList[0].text == playerSide && buttonList[1].text == playerSide && buttonList[2].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[3].text == playerSide && buttonList[4].text == playerSide && buttonList[5].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[6].text == playerSide && buttonList[7].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[0].text == playerSide && buttonList[3].text == playerSide && buttonList[6].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[1].text == playerSide && buttonList[4].text == playerSide && buttonList[7].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[2].text == playerSide && buttonList[5].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[0].text == playerSide && buttonList[4].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[2].text == playerSide && buttonList[4].text == playerSide && buttonList[6].text == playerSide)
+        {
+            GameOver();
+        }
+        if (moveCount == 9)
+        {
+            isTie = true;
+            GameOver();
+        }
+        changeSides();
+    }
+    int checkWin()
+    {
+        for (int row = 0; row < 3; row++)
+        {
+            if (board[row, 0] == board[row, 1] && board[row, 1] == board[row, 2])
+            {
+                if (board[row, 0] == "O")
+                    return 10;
+                else if (board[row, 0] == "X")
+                    return -10;
+            }
+        }
+        for (int col = 0; col < 3; col++)
+        {
+            if (board[0, col] == board[1, col] && board[1, col] == board[2, col])
+            {
+                if (board[0, col] == "O")
+                    return 10;
+                else if (board[0, col] == "X")
+                    return -10;
+            }
+        }
+        if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+        {
+            if (board[0, 0] == "O")
+                return 10;
+            else if (board[0, 0] == "X")
+                return -10;
+        }
+        if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+        {
+            if (board[0, 2] == "O")
+                return 10;
+            else if (board[0, 2] == "X")
+                return -10;
+        }
+        return 0;
+    }
+    bool isFull()
+    {
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                if (board[i, j] == "")
+                    return false;
+        return true;
+    }
+    int minimax(int depth, bool isMax)
+    {
+
+        int score = checkWin();
+
+        if (score == 10)
+            return score;
+        if (score == -10)
+            return score;
+        if (isFull())
+            return 0;
+
+        if (isMax)
+        {
+            int best = -1000;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[i, j] == "")
+                    {
+                        board[i, j] = "O";
+                        best = Math.Max(best, minimax(depth + 1, false));
+                        board[i, j] = "";
+                    }
+                }
+            }
+            return best;
+        }
+        else
+        {
+            int best = 1000;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[i, j] == "")
+                    {
+                        board[i, j] = "X";
+                        best = Math.Min(best, minimax(depth + 1, true));
+                        board[i, j] = "";
+                    }
+                }
+            }
+            return best;
+        }
+    }
+    Move findBestMove()
+    {
+        int bestVal = -1000;
+        Move bestMove;
+        bestMove.row = -1;
+        bestMove.col = -1;
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (board[i, j] == "")
+                {
+                    board[i, j] = "O";
+
+                    int moveValue = minimax(0, false);
+                    board[i, j] = "";
+                    if (moveValue > bestVal)
+                    {
+                        bestMove.row = i;
+                        bestMove.col = j;
+                        bestVal = moveValue;
+                    }
+
+                }
+            }
+        }
+        return bestMove;
+    }
+    void SetGameControllerReferenceOnButtons()
+    {
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<GridSpace>().SetGameControllerReference(this);
+        }
+    }
+    void Awake()
+    {
+        SetGameControllerReferenceOnButtons();
+        playerSide = "X";
+        gameOverPanel.SetActive(false);
+        moveCount = 0;
+    }
+    void GameOver()
+    {
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<Button>().interactable = false;
+        }
+        gameOverPanel.SetActive(true);
+        if (isTie)
+        {
+            gameOverText.text = "Tie Game!";
+        }
+        else
+        {
+            gameOverText.text = playerSide + " Wins!";
+        }
+    }
+    void changeSides()
+    {
+        playerSide = (playerSide == "X") ? "O" : "X";
+    }
+}
---
+++ GridSpace.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridSpace : MonoBehaviour
+{
+    public Button button;
+    public Text buttonText;
+    private GameController gameController;
+    private int index;
+
+    public void SetGameControllerReference(GameController controller)
+    {
+        gameController = controller;
+    }
+    public void SetSpace()
+    {
+        buttonText.text = "X";
+        button.interactable = false;
+        gameController.setMyArray();
+        gameController.EndTurn();
+        if (gameController.getPlayerSide() == "O")
+        {
+            gameController.makeAIPlay();
+        }
+    }
+}
